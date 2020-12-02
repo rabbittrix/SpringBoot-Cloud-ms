84d01a45c5e9f7f7afb369dc2eab3e1de2124365
Roberto de Souza <rabbittrix@hotmail.com>
2020-12-02 12:32:13 +0000

    hr-eureka-server created

--- hr-worker/src/main/java/com/jrsf/hrworker/resouces/WorkerResource.java
+++ hr-worker/src/main/java/com/jrsf/hrworker/resouces/WorkerResource.java
@@ -2,7 +2,10 @@ package com.jrsf.hrworker.resouces;
 
 import com.jrsf.hrworker.entities.Worker;
 import com.jrsf.hrworker.repositories.WorkerRepository;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.env.Environment;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -15,6 +18,11 @@ import java.util.List;
 @RequestMapping(value = "/workers")
 public class WorkerResource {
 
+    private static Logger logger = LoggerFactory.getLogger(WorkerResource.class);
+
+    @Autowired
+    private Environment env;
+
     @Autowired
     private WorkerRepository repository;
 
@@ -26,6 +34,7 @@ public class WorkerResource {
 
     @GetMapping(value = "/{id}")
     public ResponseEntity<Worker> findById(@PathVariable Long id){
+        logger.info("PORT = " + env.getProperty("local.server.port"));
         Worker obj = repository.findById(id).get();
         return ResponseEntity.ok(obj);
     }
